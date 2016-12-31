e3bd1e30f12226d31d86b5ed817e9ee5668ea14d
MAIMOUN Bakr <bakero.maimoun@gmail.com>
2016-12-31 16:28:22 +0000

    Add files via upload

---
+++ Blog/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$uri = $request->getPathInfo();
+var_dump($uri);
+if ('/' === $uri) {
+    $response = list_action();
+}
+elseif ('/show' === $uri && $request->query->has('id')) {
+    $response = show_action($request->query->get('id'));
+}
+elseif('/') {
+
+}
+else {
+    $html = '<html><body><h1>Page Not Found</h1></body></html>';
+    $response = new Response($html, Response::HTTP_NOT_FOUND);
+}
+
+// echo the headers and send the response
+$response->send();
+
+ ?>
---
+++ Blog/model.php
@@ -0,0 +1,46 @@
+
+
+
+
+<?php 
+
+
+function open_database_connection()
+{
+    $link = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+
+    return $link;
+}
+
+function close_database_connection(&$link)
+{
+    $link = null;
+}
+
+function get_all_posts()
+{
+    $link = open_database_connection();
+
+    $result = $link->query('SELECT id, title FROM post');
+
+    $posts = array();
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $posts[] = $row;
+    }
+    close_database_connection($link);
+
+    return $posts;
+}
+
+function get_post_by_id($id)
+{
+    $link = open_database_connection();
+    $id = intval($id);
+    $result = $link->query('SELECT created_at, title, body FROM post WHERE id = '.$id);
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+
+    close_database_connection($link);
+
+    return $row;
+}	
+ ?>
